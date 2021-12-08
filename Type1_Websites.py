c715ae0e1a42211d43a0ee0275f0c1958b4c0e83
Atalay Denknalbant <32653649+atalaydenknalbant@users.noreply.github.com>
2021-12-08 14:04:34 -0800

    Code Cleanup.

--- Type1_Websites.py
+++ Type1_Websites.py
@@ -1,7 +1,6 @@
 from selenium_codes import sub4sub_websites_selenium as sws
 from configparser import ConfigParser
 from threading import Thread
-import multiprocessing
 import os
 
 heroku = "not available"
