d6cee14e78dcef7bfb0015d112902e1eb5d28c55
Atalay Denknalbant <32653649+atalaydenknalbant@users.noreply.github.com>
2022-02-13 16:00:56 -0800

    Code Cleanup.

--- selenium_codes/sub4sub_websites_selenium.py
+++ selenium_codes/sub4sub_websites_selenium.py
@@ -1162,7 +1162,6 @@ def goviral_functions(req_dict: dict) -> None:
             try:
                 driver_9.find_element(By.XPATH, "//*[@id='kt_content']/div/div[1]/div/form/div/div[1]/div/div/button")\
                     .send_keys(Keys.ENTER)
-                # logging.info("Enable button has been pressed")
                 driver_9.refresh()
                 event.wait(2)
                 continue
@@ -1264,7 +1263,6 @@ def goviral_functions(req_dict: dict) -> None:
             try:
                 event.wait(1.5)
                 driver_9.find_element(By.ID, 'verify-action-button').click()
-                # logging.info("Clicked Verify Action Button")
                 driver_9.save_screenshot("screenshots/screenshot.png")
             except (ElementNotInteractableException, StaleElementReferenceException,
                     ElementClickInterceptedException, NoSuchElementException):
