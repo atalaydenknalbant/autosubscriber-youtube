6c2520d57ce8476c59609f153f96562549c964ff
Atalay Denknalbant <32653649+atalaydenknalbant@users.noreply.github.com>
2022-04-08 20:40:34 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='autosubscriber-youtube',
+    version='1.76',
+    packages=['selenium'],
+    url='https://github.com/atalaydenknalbant/autosubscriber-youtube',
+    license='GPL-3.0 License',
+    author='Atalay Denknalbant',
+    author_email='atalaydenknalbant@hotmail.com',
+    description='Python Selenium-based automated subscribing, liking, viewing functions for youtube sub4sub websites.'
+)
