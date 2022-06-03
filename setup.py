9d80c514bb633adb1a4208dd40c4361897d7adc9
Atalay Denknalbant <32653649+atalaydenknalbant@users.noreply.github.com>
2022-06-03 02:06:13 -0700

    Code Cleanup. Speed Improvements.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='autosubscriber-youtube',
-    version='1.92',
+    version='1.921',
     packages=['selenium', 'webdriver-manager'],
     url='https://github.com/atalaydenknalbant/autosubscriber-youtube',
     license='GPL-3.0 License',
