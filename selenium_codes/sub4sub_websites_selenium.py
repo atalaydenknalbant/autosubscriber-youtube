d3328ac9013f8ddfc8260df3767302ffd3a3fe0b
atalaydenknalbant <atalaydenknalbant@hotmail.com>
2023-03-05 21:31:55 +0300

    Fixed YTMonster where it only opens 2 videos. Added new yt_change_resolution to handle lowering YouTube video resolutions.

--- selenium_codes/sub4sub_websites_selenium.py
+++ selenium_codes/sub4sub_websites_selenium.py
@@ -94,7 +94,7 @@ def yt_change_resolution(driver: webdriver, resolution: int = 144) -> None:
         except (TimeoutException, ElementClickInterceptedException, ElementNotInteractableException,
                 StaleElementReferenceException):
             driver.refresh()
-            # logging.info("Changing Resolution Failed. Retrying...")
+            logging.error("Changing Resolution Failed. Retrying...")
 
 
 def set_driver_opt(req_dict: dict,
