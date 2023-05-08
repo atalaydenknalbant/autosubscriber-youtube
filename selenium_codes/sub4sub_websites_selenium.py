ea8b70129d238931c67c140e3df66df44ce8d9ef
atalaydenknalbant <atalaydenknalbant@hotmail.com>
2023-05-08 18:25:13 +0300

    Selenium Version Update.

--- selenium_codes/sub4sub_websites_selenium.py
+++ selenium_codes/sub4sub_websites_selenium.py
@@ -1190,14 +1190,11 @@ def youlikehits_functions(req_dict: dict) -> None:
                 break
             EVENT.wait(secrets.choice(range(3, 4)))
             driver.switch_to.window(driver.window_handles[0])
-            logging.info('Flag1')
-            driver.refresh()
             try:
                 driver.execute_script("arguments[0].click();", driver.find_element(By.CLASS_NAME, 'followbutton'))
             except (NoSuchElementException, ElementNotInteractableException, ElementClickInterceptedException,
                     JavascriptException):
                 EVENT.wait(0.25)
-                logging.info('Flag1.1')
             EVENT.wait(3.25)
             try:
                 driver.switch_to.window(driver.window_handles[1])
@@ -1227,14 +1224,11 @@ def youlikehits_functions(req_dict: dict) -> None:
                                                   'td/center/table/tbody/tr[2]/td/'
                                                   'center/div[2]/div/center/a[2]').send_keys(Keys.ENTER)
                     EVENT.wait(secrets.choice(range(1, 3)))
-                    logging.info('Flag4.2')
                     continue
                 else:
                     if i == 1:
-                        logging.info('Flag1.2')
                         i = yt_change_resolution(driver, website='YOULIKEHITS')
             except (NoSuchElementException, IndexError, NoSuchWindowException):
-                logging.info('Flag4.3')
                 EVENT.wait(0.25)
                 driver.switch_to.window(driver.window_handles[0])
                 driver.switch_to.default_content()
@@ -1263,7 +1257,6 @@ def youlikehits_functions(req_dict: dict) -> None:
             except NoSuchElementException:
                 EVENT.wait(0.25)
             EVENT.wait(secrets.choice(range(3, 5)))
-            logging.info('Flag5.1')
             try:
                 if driver.find_element(By.CSS_SELECTOR, '#listall > b').text == \
                         'There are no videos available to view at this time. Try coming back or refreshing.':
@@ -1276,28 +1269,22 @@ def youlikehits_functions(req_dict: dict) -> None:
             except NoSuchElementException:
                 pass
             z = 0
-            logging.info('Flag6')
             while len(driver.window_handles) == 1:
                 try:
                     driver.execute_script("arguments[0].click();", driver.find_element(By.CLASS_NAME, 'followbutton'))
                 except (NoSuchElementException, JavascriptException):
                     pass
-                logging.info('Flag7')
                 z += 1
                 if z == 15:
                     # driver.execute_script("window.scrollTo(0, 600);")
-                    # driver.save_screenshot("screenshots/screenshot.png")
                     try:
                         driver.find_element(By.CSS_SELECTOR, '#listall > center > a:nth-child(11)').click()
                     except (NoSuchElementException, ElementNotInteractableException):
                         break
                     driver.execute_script("arguments[0].click();", driver.find_element(By.CLASS_NAME, 'followbutton'))
-                    logging.info('Flag8')
                     if z == 15:
                         j += 1
-                logging.info('Flag8.9')
             EVENT.wait(secrets.choice(range(3, 4)))
-            logging.info('Flag9')
             # driver.execute_script("window.scrollTo(0, 600);")
 
     while_loop_watch(14)
